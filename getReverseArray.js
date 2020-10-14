8faed85961eb9db6a768a9cdcda554de1127ce7c
IamLeTea <iamletea@gmail.com>
2020-10-14 14:20:42 +0800

    Functions: Create getReverseArray function & test.

---
+++ getReverseArray.js
@@ -0,0 +1,12 @@
+// local files
+const checkIsArray = require("./checkIsArray");
+
+const getReverseArray = array => {
+  if (!checkIsArray(array)) {
+    throw new Error("The parameter is not array.");
+  }
+
+  return array.reverse();
+};
+
+module.exports = getReverseArray;
