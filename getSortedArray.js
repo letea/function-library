ede302656a5457fc488facabc0ecbf773c39265b
IamLeTea <iamletea@gmail.com>
2020-10-14 14:38:01 +0800

    Functions: Create getSortedArray function & test.

---
+++ getSortedArray.js
@@ -0,0 +1,12 @@
+// local files
+const checkIsArray = require("./checkIsArray");
+
+const getSortedArray = array => {
+  if (!checkIsArray(array)) {
+    throw new Error("The parameter is not array.");
+  }
+
+  return array.sort();
+};
+
+module.exports = getSortedArray;
---
+++ getSortedArray.test.js
@@ -0,0 +1,25 @@
+// local modules - jest
+const runTest = require("./jest/runTest");
+
+// local files
+const getSortedArray = require("./getSortedArray");
+
+const testCases = [
+  {
+    describe: "should return an sorted array",
+    it: "if the argument is valid.",
+    toBe: [1, 2, 2, 3, 3, 5],
+    cases: [[1, 2, 3, 2, 3, 5], [5, 1, 3, 2, 3, 2]]
+  }
+];
+
+const handler = ({ testCase, item }) => {
+  expect(getSortedArray(item)).toEqual(testCase.toBe);
+};
+
+runTest({ testCases, handler });
+
+module.exports = {
+  testCases,
+  handler
+};
